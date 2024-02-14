d75ce828fcb3da5015ed503875a1996e7624d851
Maksim <maksim2335@yandex.ru>
2024-02-14 22:55:01 +0300

    Инициализация проекта

---
+++ src/test/java/com/maksim2335/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksim2335.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
