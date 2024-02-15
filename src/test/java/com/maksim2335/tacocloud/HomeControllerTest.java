1fae02e11d6007eb2cfcab7efc97ff624d9ecfa3
Maksim <maksim2335@yandex.ru>
2024-02-15 20:03:48 +0300

    Контроллер предствления Home добавлке в загрузочный класс

--- src/test/java/com/maksim2335/tacocloud/HomeControllerTest.java
+++ src/test/java/com/maksim2335/tacocloud/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package com.maksim2335.tacocloud;
 
-import com.maksim2335.tacocloud.Controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,7 +8,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
