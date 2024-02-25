60b36171bab669352cac5d577f2e4455631de67b
Maksim <maksim2335@yandex.ru>
2024-02-25 23:14:11 +0300

    Добавлена реализация интерфеса Persistable для понимания новой сущности

--- src/main/java/com/maksim2335/tacocloud/Model/Ingredient.java
+++ src/main/java/com/maksim2335/tacocloud/Model/Ingredient.java
@@ -2,15 +2,22 @@ package com.maksim2335.tacocloud.Model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table
-public class Ingredient {
+public class Ingredient implements Persistable<String> {
     @Id
     private final String id;
     private final String name;
     private final Type type;
+
+    @Override
+    public boolean isNew() {
+        return true;
+    }
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
