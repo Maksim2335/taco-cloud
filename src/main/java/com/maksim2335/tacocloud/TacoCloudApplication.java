0310b1ccf8b2ac4f50b8150bd35498814a666f1e
Maksim <maksim2335@yandex.ru>
2024-03-05 22:59:59 +0300

    Настрона защита веб-запросов

--- src/main/java/com/maksim2335/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/maksim2335/tacocloud/TacoCloudApplication.java
@@ -19,6 +19,7 @@ public class TacoCloudApplication implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/login");
 	}
 
 	@Bean
--- src/main/java/com/maksim2335/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/maksim2335/tacocloud/security/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.maksim2335.tacocloud.security;
 import com.maksim2335.tacocloud.Repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -35,6 +37,28 @@ public class SecurityConfig {
             if (user != null) return user;
             throw new UsernameNotFoundException("User ‘" + username + "’ not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/design", "/orders").hasRole("USER")
+                        .requestMatchers("/", "/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLoginConfigurer -> formLoginConfigurer
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/design"))
+//                .oauth2Login(new Customizer<OAuth2LoginConfigurer<HttpSecurity>>() {
+//                    @Override
+//                    public void customize(OAuth2LoginConfigurer<HttpSecurity> httpSecurityOAuth2LoginConfigurer) {
+//                        httpSecurityOAuth2LoginConfigurer.loginPage()
+//                    }
+//                })
+                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
+                        .permitAll()
+                        .logoutSuccessUrl("/"))
+                .build();
     }
 
 }
