7d3d224e9568f017b0abc5eb4dcc1f00ffda3db4
Maksim <maksim2335@yandex.ru>
2024-03-08 17:24:26 +0300

    Добавлена идентификация пользователя из spring security

--- src/main/java/com/maksim2335/tacocloud/Controller/OrderController.java
+++ src/main/java/com/maksim2335/tacocloud/Controller/OrderController.java
@@ -1,10 +1,12 @@
 package com.maksim2335.tacocloud.Controller;
 
 import com.maksim2335.tacocloud.Model.TacoOrder;
+import com.maksim2335.tacocloud.Model.User;
 import com.maksim2335.tacocloud.Repository.OrderRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,10 +32,12 @@ public class OrderController {
 
     @PostMapping
     public String processOrder(@Valid TacoOrder order, Errors errors,
-                               SessionStatus sessionStatus) {
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()){
             return "orderForm";
         }
+        order.setUser(user);
         orderRepository.save(order);
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
--- src/main/java/com/maksim2335/tacocloud/Model/TacoOrder.java
+++ src/main/java/com/maksim2335/tacocloud/Model/TacoOrder.java
@@ -49,6 +49,9 @@ public class TacoOrder {
     @OneToMany (cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
+    @ManyToOne
+    private User user;
+
     public void addTaco(Taco taco) {
         this.tacos.add(taco);
     }
