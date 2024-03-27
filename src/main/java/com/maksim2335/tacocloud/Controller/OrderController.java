b383bf0302436851b9ac11eb6e697695b63e6e9f
Maksim <maksim2335@yandex.ru>
2024-03-27 22:51:44 +0300

    БЫли созданы собственные конфигурационные свойства для будущего функцционала по отображению списка закзов

--- src/main/java/com/maksim2335/tacocloud/Controller/OrderController.java
+++ src/main/java/com/maksim2335/tacocloud/Controller/OrderController.java
@@ -2,20 +2,22 @@ package com.maksim2335.tacocloud.Controller;
 
 import com.maksim2335.tacocloud.Model.TacoOrder;
 import com.maksim2335.tacocloud.Model.User;
+import com.maksim2335.tacocloud.Props.OrderProps;
 import com.maksim2335.tacocloud.Repository.OrderRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-
-import java.util.Date;
 
 @Slf4j
 @Controller
@@ -24,10 +26,24 @@ import java.util.Date;
 public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
+    private final OrderProps props;
+
+    public OrderController(OrderProps props) {
+        this.props = props;
+    }
 
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @GetMapping
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders",
+                orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
     @PostMapping
--- src/main/java/com/maksim2335/tacocloud/Repository/OrderRepository.java
+++ src/main/java/com/maksim2335/tacocloud/Repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.maksim2335.tacocloud.Repository;
 
 import com.maksim2335.tacocloud.Model.TacoOrder;
+import com.maksim2335.tacocloud.Model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
@@ -13,5 +15,7 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     List<TacoOrder> findByDeliveryNameAndDeliveryCityAllIgnoreCase(
             String deliveryName, String deliveryCity);
     List<TacoOrder> findByDeliveryCityOrderByDeliveryName(String city);
+
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 
 }
